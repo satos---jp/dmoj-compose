e86468a46030e1cc2223f2c7ee620124cea9d7be
satos---jp <syogo8118@yahoo.co.jp>
2023-12-02 06:15:04 +0900

    clean up users and I think we can do 1st debug

--- password_hash.py
+++ password_hash.py
@@ -6,7 +6,8 @@ ty = 'sha256'
 # N = 1
 # salt = b''
 salt = b'eFRRZq4DgktS'
-h = hashlib.pbkdf2_hmac(ty, b'foo31415', salt, N)
+salt = b't3Ct3t3RF23r'
+h = hashlib.pbkdf2_hmac(ty, b'y3432r334gh3455v445fg344g45hgverdbveg34fw', salt, N)
 print(h.hex(),len(h.hex()))
 h = base64.b64encode(h).decode('ascii')
 print(f"pbkdf2_{ty}${N}${salt.decode('ascii')}${h}")
